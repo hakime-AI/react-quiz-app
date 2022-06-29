9562769f1995df2a0b9b556698b1bd1e2cc68898
hakime-AI <hakimeattoumani.simplon@gmail.com>
2022-06-29 16:27:35 +0200

    add mapping

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import shuffle from './Shuffle';
 
-const Card = ({question,answers}) => {
-    
+const Card = ({ question, questionId,handleResponse,correctAnswer,incorrectAnswers }) => {
+    const [responces,setResponces] = useState([]);
+    useEffect(()=>{
+       setResponces(shuffle(incorrectAnswers.concat(correctAnswer)))
+    },[]);
+    // const shuffledAnswres = shuffle(answers);
     return (
         <div className='card'>
             <div className='question'>
-            <h2>{question.question}</h2>
-            {answers.map((answer,index)=><p key={index}>{answer}</p>)}
-            </div>
-            <div className='responce'>
-
+                <h2>{question.question}</h2>
+                {responces.map((answer, index) =>
+                    <div key={questionId + index}>
+                        <input type="radio" id={questionId + index} name={questionId} onChange={()=>handleResponse(answer,correctAnswer)}/>
+                        <label htmlFor={questionId + index}>{answer}</label>
+                    </div>)}
             </div>
         </div>
     );
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -4,6 +4,9 @@ import Card from './Card';
 
 const Questions = () => {
     const [data,setData] = useState([]);
+    const [score, setScore] = useState(0);
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    // const [responces, setResponces] = useState([]);
     //useEffect se joue lorsque le composant est construit
     useEffect(()=>{
         axios
@@ -11,21 +14,42 @@ const Questions = () => {
         .then((res)=>setData(res.data))
     },[]);
 
-       
+
+
+    const handleResponse = (a,b) => {
+
+        if (a===b) {
+           setScore(current => current + 1); 
+        }
+        setCurrentQuestion(current => current + 1); 
+        // 👇️ take parameter passed from Child component
+      };
+
+      
     
     //https://restcountries.com/v3.1/all
     return (
             <div className="Questions">
-                <h1>Questions</h1>
+                <h1>Score : {score}</h1>
+                <h2>Question : {currentQuestion}</h2>
                 <div>
                     {
-                    data.map((question)=> (
-                        <Card key={question.id} question={question} answers={question.incorrectAnswers.concat(question.correctAnswer)}/>
+                    data.slice(currentQuestion,currentQuestion+1).map((question)=> (
+                        <Card
+                        key={question.id}
+                        question={question}
+                        incorrectAnswers={question.incorrectAnswers}
+                        correctAnswer={question.correctAnswer}
+                        questionId={question.id}
+                        handleResponse={handleResponse}
+                        />
                         ))
                     }
                 </div>
             </div>
     );
 };
+
+
 
 export default Questions;
